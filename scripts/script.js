1ca5e430ab5cfa937155c98d0f7101be87bdfe96
gerontivero <gerbonti377@gmail.com>
2019-09-29 00:03:00 -0300

    dos idiomas en index.html

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,69 @@
 function OnClickFlag(aFlag){
     if(aFlag == 'Spanish'){
+        document.getElementById("enNavBar").style.display = "none";
         document.getElementById("enNosotros").style.display = "none";
+        document.getElementById("enCoresat").style.display = "none";
+        document.getElementById("enARTI").style.display = "none";
+        document.getElementById("enCoheteria").style.display = "none";
+        document.getElementById("enDebris").style.display = "none";
+        document.getElementById("enSumo").style.display = "none";
+        document.getElementById("enRSL").style.display = "none";
+        document.getElementById("enVRTD").style.display = "none";
+        document.getElementById("enRSLuz").style.display = "none";
+        document.getElementById("enProyectos").style.display = "none";
+        document.getElementById("enParticipa").style.display = "none";
+        document.getElementById("enReuniones").style.display = "none";
+        document.getElementById("enRobots").style.display = "none";
+        document.getElementById("enAero").style.display = "none";
+        
+        document.getElementById("spNavBar").style.display = "block";
         document.getElementById("spNosotros").style.display = "block";
+        document.getElementById("spCoresat").style.display = "block";
+        document.getElementById("spARTI").style.display = "block";
+        document.getElementById("spCoheteria").style.display = "block";
+        document.getElementById("spDebris").style.display = "block";
+        document.getElementById("spSumo").style.display = "block";
+        document.getElementById("spRSL").style.display = "block";
+        document.getElementById("spSumo").style.display = "block";
+        document.getElementById("spRSL").style.display = "block";
+        document.getElementById("spProyectos").style.display = "block";
+        document.getElementById("spParticipa").style.display = "block";
+        document.getElementById("spReuniones").style.display = "block";
+        document.getElementById("spRobots").style.display = "block";
+        document.getElementById("spAero").style.display = "block";
     }
     else{
+        document.getElementById("enNavBar").style.display = "block";
         document.getElementById("enNosotros").style.display = "block";
+        document.getElementById("enCoresat").style.display = "block";
+        document.getElementById("enARTI").style.display = "block";
+        document.getElementById("enCoheteria").style.display = "block";
+        document.getElementById("enDebris").style.display = "block";
+        document.getElementById("enSumo").style.display = "block";
+        document.getElementById("enRSL").style.display = "block";
+        document.getElementById("enVRTD").style.display = "block";
+        document.getElementById("enRSLuz").style.display = "block";
+        document.getElementById("enProyectos").style.display = "block";
+        document.getElementById("enParticipa").style.display = "block";
+        document.getElementById("enReuniones").style.display = "block";
+        document.getElementById("enRobots").style.display = "block";
+        document.getElementById("enAero").style.display = "block";
+        
+        
+        document.getElementById("spNavBar").style.display = "none";
         document.getElementById("spNosotros").style.display = "none";
+        document.getElementById("spCoresat").style.display = "none";
+        document.getElementById("spARTI").style.display = "none";
+        document.getElementById("spCoheteria").style.display = "none";
+        document.getElementById("spDebris").style.display = "none";
+        document.getElementById("spSumo").style.display = "none";
+        document.getElementById("spRSL").style.display = "none";
+        document.getElementById("spVRTD").style.display = "none";
+        document.getElementById("spRSLuz").style.display = "none";
+        document.getElementById("spProyectos").style.display = "none";
+        document.getElementById("spParticipa").style.display = "none";
+        document.getElementById("spReuniones").style.display = "none";
+        document.getElementById("spRobots").style.display = "none";
+        document.getElementById("spAero").style.display = "none";
     }
 }
